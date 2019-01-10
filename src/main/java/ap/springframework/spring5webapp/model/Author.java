a2bae26079a80277bde2caeefc9b3e3f75e50d1e
Alessandro Pilonato <alessandro.pilonato@ptqs.it>
2019-01-10 08:15:36 +0100

    Added application.properties and equals on model objects

--- src/main/java/ap/springframework/spring5webapp/model/Author.java
+++ src/main/java/ap/springframework/spring5webapp/model/Author.java
@@ -1,13 +1,8 @@
 package ap.springframework.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity
 public class Author {
