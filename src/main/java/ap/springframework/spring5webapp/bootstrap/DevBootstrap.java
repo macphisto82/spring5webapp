58cc47bc6f4d7da95504d2d828cb678e5685e4bc
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-10 14:00:07 +0100

    Add Publisher model, repo and change relation with Book

--- src/main/java/ap/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/ap/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,18 +6,22 @@ import org.springframework.stereotype.Component;
 
 import ap.springframework.spring5webapp.model.Author;
 import ap.springframework.spring5webapp.model.Book;
+import ap.springframework.spring5webapp.model.Publisher;
 import ap.springframework.spring5webapp.repositories.AuthorRepository;
 import ap.springframework.spring5webapp.repositories.BookRepository;
+import ap.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	@Override
@@ -27,9 +31,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
 	private void initData(){
 
+		Publisher publisher = new Publisher();
+        publisher.setName("foo");
+        publisher.setAddress("12th Street, LA");
+        publisherRepository.save(publisher);
+        
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book  ddd = new Book("Domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -38,7 +47,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
         
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher );
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
         
